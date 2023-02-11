120c89329716e9b47ea57e82b21b05863a4ac13b
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-11 14:29:19 -0400

    Definir vistas del menu e incluir las URLs

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(req):
+    return HttpResponse("Home")
+
+def servicios(req):
+    return HttpResponse("Servicios")
+
+def tienda(req):
+    return HttpResponse("Tienda")
+
+def blog(req):
+    return HttpResponse("Blog")
+
+def contacto(req):
+    return HttpResponse("Contacto")

0db826a0444dff76ffeecdd4facfa48702b0a2a3
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-16 21:29:17 -0400

    filtrar los Post por categoria

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
-from blog.models import Post
+from blog.models import Post, Categoria
 # Create your views here.
 
 def blog(req):
     posts = Post.objects.all()
     return render(req, "blog/blog.html", {"posts": posts})
+
+
+def categoria(req, categoria_id):
+    categoria=Categoria.objects.get(id=categoria_id)
+    posts = Post.objects.filter(categorias=categoria)
+    return render(req, "blog/categorias.html", {"categoria": categoria, "posts": posts})

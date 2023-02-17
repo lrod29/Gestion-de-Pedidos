4d5af83eb3d74896c175772dd38cdfea06fbaabe
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-17 16:13:48 -0400

    metodo POST y paso de parametros usando GET

--- contacto/views.py
+++ contacto/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioContacto
 
 # Create your views here.
 
 def contacto(req):
     formulario_contacto=FormularioContacto()
+    if req.method=="POST":
+        formulario_contacto=FormularioContacto(data=req.POST)
+        if formulario_contacto.is_valid():
+            nombre=req.POST.get("nombre")
+            email=req.POST.get("email")
+            contenido=req.POST.get("contenido")
+            return redirect("/contacto/?valido")
+
     return render(req, "contacto/contacto.html",{"miFormulario": formulario_contacto})

28fca391e3924e1c63fdbd5164d27b0e71d113c6
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-17 14:22:09 -0400

    formato al formulario de contacto

--- contacto/forms.py
+++ contacto/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre=forms.CharField(label="Nombre", max_length=50, required=True)
     email=forms.CharField(label="Email", max_length=50, required=True)
-    contenido=forms.CharField(label="Contenido")
+    contenido=forms.CharField(label="Contenido", widget=forms.Textarea)

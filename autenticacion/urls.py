e082fedad302c00a963092e038eaa9438f8139e7
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-19 14:15:25 -0400

    validar formulario de login

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import Vregistro, cerrar_sesion
+from .views import Vregistro, cerrar_sesion, loginUser
 
 
 urlpatterns = [
 
     path('', Vregistro.as_view(), name="autenticacion"),
     path('cerrar_sesion', cerrar_sesion, name="cerrar_sesion"),
+    path('login_user', loginUser, name="login_user"),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
@@ -27,3 +27,22 @@ class Vregistro(View):
 def cerrar_sesion(request):
     logout(request)
     return redirect('Home')
+
+def loginUser(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario=form.cleaned_data.get("username")
+            contra=form.cleaned_data.get("password")
+            usuario=authenticate(username=nombre_usuario, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('Home')
+            else:
+                messages.error(request, "Usuario no valido")
+        else:
+            messages.error(request, "Algo va mal")
+
+    form=AuthenticationForm()
+    return render(request, "login/login.html", {"form":form})
+    

ffc4e05dc506320142f7be6b0f30e29696c82260
Luis Rodriguez <luis29.rod@gmail.com>
2023-02-19 13:52:03 -0400

    reconocimiento de usuario registrado y cerrar la sesion de usuario

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import Vregistro
+from .views import Vregistro, cerrar_sesion
 
 
 urlpatterns = [
 
     path('', Vregistro.as_view(), name="autenticacion"),
+    path('cerrar_sesion', cerrar_sesion, name="cerrar_sesion"),
 ]
